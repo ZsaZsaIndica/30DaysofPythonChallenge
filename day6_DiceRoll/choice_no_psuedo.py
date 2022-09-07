e3d66d0324347627739268eda6da62a90a7cbc03
ZsaZsaIndica <113045938+ZsaZsaIndica@users.noreply.github.com>
2022-09-07 09:17:08 -0700

    improving user flow control
    user is now able to remain with the dice they have initially chosen

--- day6_DiceRoll/choice_no_psuedo.py
+++ day6_DiceRoll/choice_no_psuedo.py
@@ -1,12 +1,6 @@
-#  --- this script allows user to choose between several different dice options ---
 
-
-# import necessary libraries
 import random2
 
-# define a dictionary to hold key value pairs of dice information.
-# dice can be any size needed. range of the dice number options (value) must match dice name (key)
-# player is able to choose from different dice options
 dice_options = {
       "dice6": (1,6),
       "dice18": (1,18),
@@ -14,18 +8,14 @@ dice_options = {
       "dice60": (1,60)
     }
 
-# invite user to roll dice
 game_play = input('\nShall we roll some dice? type yes or no: ')
-# user inputs which dice needed
 dice_chosen = input("\nPlease choose your dice from the following options:\n dice6, dice18, dice34 or dice60: ").lower()
-# match the dice chosen to the dice/range in the dictionary and assign to a new variable
 user_dice = dice_chosen
 let_the_dice_roll = True
 while let_the_dice_roll:
   if game_play == "yes":
     dice_being_rolled = dice_options[dice_chosen]
-# using the randint method fromt the random module, a random integer is chosen and printed to the console
-    print("\nThe die has been cast and your number is\n: ", random2.randint(dice_being_rolled[0], dice_being_rolled[1]))
+    print("\nThe die is cast and your number is\n: ", random2.randint(dice_being_rolled[0], dice_being_rolled[1]))
     dice_continue = (input("\nPress enter to roll again. \n Or \nType \"see ya\" to leave the game. ").lower())
     if dice_continue == "y":
       user_dice = True
